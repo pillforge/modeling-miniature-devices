d3b0d19892a23eeb44966fd065261f7c94efc8b8
Hakan Tunc <hakan.tunc@vanderbilt.edu>
2016-03-10 13:31:48 -0600

    Save Makefile and application files to a tmp folder

---
+++ src/utils/Util.js
@@ -0,0 +1,31 @@
+define(['module', 'path', 'tmp', 'fs-extra', 'dot'], function (module, path, tmp, fs, dot) {
+  'use strict';
+
+  var template_library_path = path.join(module.uri, '..', '..', 'template-library');
+
+  return {
+    compileApplication: compileApplication
+  };
+
+  function compileApplication (obj) {
+    var tmpobj = tmp.dirSync();
+    var config_nc = _compileTemplate('Application/ApplicationAppC.nc.dot', obj);
+    var module_nc = _compileTemplate('Application/ApplicationC.nc.dot', obj);
+    var makefile = _compileTemplate('Application/Makefile.dot', obj);
+    fs.outputFileSync(path.join(tmpobj.name, obj.name + 'AppC.nc'), config_nc);
+    fs.outputFileSync(path.join(tmpobj.name, obj.name + 'C.nc'), module_nc);
+    fs.outputFileSync(path.join(tmpobj.name, 'Makefile'), makefile);
+    return tmpobj;
+  }
+
+  function _compileTemplate (template_path, obj) {
+    return _getDotTemplate(template_path)(obj);
+  }
+
+  function _getDotTemplate (template_path) {
+    dot.templateSettings.strip = false;
+    template_path = path.join(template_library_path, template_path);
+    return dot.template(fs.readFileSync(template_path));
+  }
+
+});
---
+++ test/utils/Util.spec.js
@@ -0,0 +1,25 @@
+describe('Util', function() {
+  'use strict';
+
+  var testFixture = require('../globals');
+  var expect = testFixture.expect;
+  var path = require('path');
+  var fs = require('fs-extra');
+  var util = testFixture.requirejs('src/utils/Util');
+
+  describe('#compileApplication', function() {
+    it('should generate barebone Application', function() {
+      var tmpobj = util.compileApplication({name: 'Test'});
+      [ 'TestC.nc',
+        'TestAppC.nc',
+        'Makefile'
+      ].forEach(file => {
+        var f_path = path.join(tmpobj.name, file);
+        expect(fs.existsSync(f_path)).to.equal(true, f_path + ' should exist');
+      });
+      var appc_nc = fs.readFileSync(path.join(tmpobj.name, 'TestAppC.nc'), 'utf8');
+      appc_nc.should.contain('configuration TestAppC');
+      appc_nc.should.contain('App.Boot -> MainC');
+    });
+  });
+});
